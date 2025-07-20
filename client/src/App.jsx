b0d5bff36a12e64acaf0351c0a02434435f80a2e
meetmandawat13 <meetmandawat7@gmail.com>
2025-07-20 22:11:02 +0530

    updated the error of file import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import {useLocation , Routes , Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import CarDetails from "./pages/CarDetails";
